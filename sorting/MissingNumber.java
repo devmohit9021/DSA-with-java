a02684641d09c3a209d481a5eb2bea2eefe27958
name <mohitbare711@gmail.com>
2025-11-04 00:15:52 +0530

    Implement Missing Number algorithm to find the missing integer in an array

---
+++ sorting/MissingNumber.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    public int MissingNumber(int[] arr) {
+
+        int i = 0;
+
+        while (i < arr.length) {
+            int correct = arr[i];
+
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            } else {
+                i++;
+            }
+
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index] != index){
+                return index;
+            }
+        }
+
+        return arr.length;
+
+    }
+
+}
