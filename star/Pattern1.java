4d56856d3d81cb1129953947e32fe9a4c8dc7be2
name <mohitbare711@gmail.com>
2025-11-17 22:36:19 +0530

    Implement Pattern1 class to print a right-angled triangle of asterisks

---
+++ star/Pattern1.java
@@ -0,0 +1,17 @@
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        patt1(5);
+
+    }
+
+    static void patt1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
